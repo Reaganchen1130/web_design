c7fafabadb6596442fcc8b36d950fcd92543249a
korverchen <tobychen5487@gmail.com>
2019-07-09 21:08:19 +0800

    use admin panel

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,2 +1,10 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i>
+        <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-user-md"></i>
+        <span>User</span></a></li>
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-product-hunt"></i>
+        <span>Product</span></a></li>
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-clipboard	"></i>
+        <span>Order</span></a></li>
