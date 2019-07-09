b37dd5c148e35f240cbca99684dd388aad2dc2e1
korverchen <tobychen5487@gmail.com>
2019-07-09 12:12:49 +0800

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,9 @@
 @extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <div class="row justify-content">
+        @include('layouts.member_sidebar')
+    </div>
+</div>
+@endsection
